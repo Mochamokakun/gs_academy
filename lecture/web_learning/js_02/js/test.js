f33909c4fccd9f7a22460fd8a6270da37eee2fc8
Mochamokakun <koheisumioka444@gmail.com>
2022-10-25 16:02:48 +0900

    資料のUP

---
+++ lecture/web_learning/js_02/js/test.js
@@ -0,0 +1,13 @@
+// $("body").append("<div>ABC</div>")
+
+const link = '<a href="#">リンク</a>';
+// $("#a1").html(link)
+// $("#a1").text(link);
+// $("#a1").css("color","#ff0000");
+// $("#a1").css("background-color","#000000");
+// $("#a1").fadeOut(1000).fadeIn(3000);
+
+$("#btn").on("click",function(){
+    // alert("ok");
+    $("body").css("background-color","#000000");
+})
