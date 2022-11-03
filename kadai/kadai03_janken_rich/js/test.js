2ed93b141b7540b6376fca1eb430c7feac16db13
Mochamokakun <koheisumioka444@gmail.com>
2022-11-04 04:39:52 +0900

    表作成優先のために､コード変更

--- kadai/kadai03_janken_rich/js/test.js
+++ kadai/kadai03_janken_rich/js/test.js
@@ -1,7 +1,7 @@
 //演習jQuery
 //$→　jQueryを指している
 let i =0;
-
+var array=[];
 $("#gu_btn").on('click',function(){
     i++;    
     var random = Math.floor(Math.random() * 3);    
@@ -9,21 +9,17 @@ $("#gu_btn").on('click',function(){
         $("#user_hands").text("グー");
         $("#pc_hands").text("グー");
         $("#judgment").text("引き分け");
-        var array = [{user_hands:$("#user_hands").text()},{pc_hands:$("#pc_hands").text()},{judgment:$("#judgment").text()},{cnt:"1"}];
-        localStorage.setItem(i,JSON.stringify(array));
-
+        array.push({user_hands:$("#user_hands").text(),pc_hands:$("#pc_hands").text(),judgment:$("#judgment").text(),cnt:"1"});
     } else if (random === 1) {
         $("#user_hands").text("グー");
         $("#pc_hands").text("チョキ");
         $("#judgment").text("負け");
-        var array = [{user_hands:$("#user_hands").text()},{pc_hands:$("#pc_hands").text()},{judgment:$("#judgment").text()},{cnt:"1"}];
-        localStorage.setItem(i,JSON.stringify(array));
+        array.push({user_hands:$("#user_hands").text(),pc_hands:$("#pc_hands").text(),judgment:$("#judgment").text(),cnt:"1"});
     } else if (random === 2) {
         $("#user_hands").text("グー");
         $("#pc_hands").text("パー");
         $("#judgment").text("勝ち");
-        var array = [{user_hands:$("#user_hands").text()},{pc_hands:$("#pc_hands").text()},{judgment:$("#judgment").text()},{cnt:"1"}];
-        localStorage.setItem(i,JSON.stringify(array));
+        array.push({user_hands:$("#user_hands").text(),pc_hands:$("#pc_hands").text(),judgment:$("#judgment").text(),cnt:"1"});
         } 
 })
 $("#cho_btn").on('click',function(){
@@ -33,20 +29,17 @@ $("#cho_btn").on('click',function(){
         $("#user_hands").text("チョキ");
         $("#pc_hands").text("グー");
         $("#judgment").text("負け");
-        var array = [{user_hands:$("#user_hands").text()},{pc_hands:$("#pc_hands").text()},{judgment:$("#judgment").text()},{cnt:"1"}];
-        localStorage.setItem(i,JSON.stringify(array));
+        array.push({user_hands:$("#user_hands").text(),pc_hands:$("#pc_hands").text(),judgment:$("#judgment").text(),cnt:"1"});
     } else if (random === 1) {
         $("#user_hands").text("チョキ");
         $("#pc_hands").text("チョキ");
         $("#judgment").text("引き分け");
-        var array = [{user_hands:$("#user_hands").text()},{pc_hands:$("#pc_hands").text()},{judgment:$("#judgment").text()},{cnt:"1"}];
-        localStorage.setItem(i,JSON.stringify(array));
+        array.push({user_hands:$("#user_hands").text(),pc_hands:$("#pc_hands").text(),judgment:$("#judgment").text(),cnt:"1"});
     } else if (random === 2) {
         $("#user_hands").text("チョキ");
         $("#pc_hands").text("パー");
         $("#judgment").text("勝ち");
-        var array = [{user_hands:$("#user_hands").text()},{pc_hands:$("#pc_hands").text()},{judgment:$("#judgment").text()},{cnt:"1"}];
-        localStorage.setItem(i,JSON.stringify(array));
+        array.push({user_hands:$("#user_hands").text(),pc_hands:$("#pc_hands").text(),judgment:$("#judgment").text(),cnt:"1"});
     } 
 })
 $("#par_btn").on('click',function(){
@@ -56,37 +49,42 @@ $("#par_btn").on('click',function(){
         $("#user_hands").text("パー");
         $("#pc_hands").text("グー");
         $("#judgment").text("勝ち");
-        var array = [{user_hands:$("#user_hands").text()},{pc_hands:$("#pc_hands").text()},{judgment:$("#judgment").text()},{cnt:"1"}];
-        localStorage.setItem(i,JSON.stringify(array));
+        array.push({user_hands:$("#user_hands").text(),pc_hands:$("#pc_hands").text(),judgment:$("#judgment").text(),cnt:"1"});
     } else if (random === 1) {
         $("#user_hands").text("パー");
         $("#pc_hands").text("チョキ");
         $("#judgment").text("負け");
-        var array = [{user_hands:$("#user_hands").text()},{pc_hands:$("#pc_hands").text()},{judgment:$("#judgment").text()},{cnt:"1"}];
-        localStorage.setItem(i,JSON.stringify(array));
+        array.push({user_hands:$("#user_hands").text(),pc_hands:$("#pc_hands").text(),judgment:$("#judgment").text(),cnt:"1"});
     } else if (random === 2) {
         $("#user_hands").text("パー");
         $("#pc_hands").text("パー");
         $("#judgment").text("引き分け");
-        var array = [{user_hands:$("#user_hands").text()},{pc_hands:$("#pc_hands").text()},{judgment:$("#judgment").text()},{cnt:"1"}];
-        localStorage.setItem(i,JSON.stringify(array));
+        array.push({user_hands:$("#user_hands").text(),pc_hands:$("#pc_hands").text(),judgment:$("#judgment").text(),cnt:"1"});
     } 
 }) 
 
 $("#result").on('click',function(){
-    var pivot_array=[];
-    for(let n =0; n<localStorage.length; n++){
-        let key = localStorage.key(n);
-        var array_result = JSON.parse(localStorage.getItem(key));
-        pivot_array.push(array_result);
-    }
-    $('#get_data').append(pivot_array);
-    console.log(pivot_array);
+    console.log(array);
+    var players =array
+    const playerList = document.getElementById("playerList");
+
+    players.forEach((player) => {  // 配列の中のオブジェクトの数だけ処理を繰り返す
+    const tr = document.createElement("tr");  
+      playerList.appendChild(tr); // 表の中に８個の「tr」（行）ができる
+      // 1行の中を生成
+      const objArray = Object.entries(player);  // オブジェクトを配列に
+      objArray.forEach((arr) => { // No, name, age, gradeの4回繰り返す
+        const td = document.createElement("td");
+        td.textContent = arr[1];  // arr[1]はvalueの部分
+        tr.appendChild(td);
+    });
+    });
 })
 
 $("#reset").on('click',function(){
     localStorage.clear();
     i =0;
+    $("#playerList").text("");
     $("#user_hands").text("なに?");
     $("#pc_hands").text("なに?");
     $("#judgment").text("???");
